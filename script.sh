837a983ac1957d53db804c5ae2baf646f46ca6eb
Li Dehua <lidehua2005@gmail.com>
2025-03-17 11:09:06 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,20 +2,22 @@
 cd openwrt
 
 # Add luci-app-adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package-temp/luci-app-adguardhome
-mv -f package-temp/luci-app-adguardhome package/lean/
-rm -rf package-temp
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package-temp/luci-app-adguardhome
+#mv -f package-temp/luci-app-adguardhome package/lean/
+#rm -rf package-temp
 
 # Add luci-theme-opentomcat
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git theme-temp/luci-theme-opentomcat
-rm -rf theme-temp/luci-theme-opentomcat/LICENSE
-rm -rf theme-temp/luci-theme-opentomcat/README.md
-mv -f theme-temp/luci-theme-opentomcat package/lean/
-rm -rf theme-temp
-default_theme='opentomcat'
-sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
+#git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git theme-temp/luci-theme-opentomcat
+#rm -rf theme-temp/luci-theme-opentomcat/LICENSE
+#rm -rf theme-temp/luci-theme-opentomcat/README.md
+#mv -f theme-temp/luci-theme-opentomcat package/lean/
+#rm -rf theme-temp
+#default_theme='opentomcat'
+#sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
 
 # Add luci-app-amlogic
-git clone https://github.com/ophub/luci-app-amlogic.git  package-temp/luci-app-amlogic
-mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/lean/
-rm -rf package-temp
+#git clone https://github.com/ophub/luci-app-amlogic.git  package-temp/luci-app-amlogic
+#mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/lean/
+#rm -rf package-temp
+
+echo "script done"
